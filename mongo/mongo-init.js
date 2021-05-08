0b190f4b6f528e9d60406340a0486f471177efa7
randomnanastya <randomnanastya@gmail.com>
2021-05-08 16:04:06 +0300

    добавлены данные в бд о цветах после запуска докера

--- mongo/mongo-init.js
+++ mongo/mongo-init.js
@@ -1,4 +1,4 @@
-//db = db.getSiblingDB('DevTodoDB');
+db = db.getSiblingDB('DevTodoDB');
 
 //db.createUser(
 //  {
@@ -8,12 +8,12 @@
 //  },
 //);
 
-//db.createCollection('colors');
-//
-//db.colors.insertMany([
-//    {"color": "Black", "hex_color": "#000000"},
-//    {"color": "Grey", "hex_color": "#bababa"},
-//    {"color": "Dark grey", "hex_color": "#b2b2b2"},
-//    {"color": "Orange", "hex_color": "#ffa500"},
-//    {"color": "Dark navy", "hex_color": "#576675"}
-//])
+db.createCollection('colors');
+
+db.colors.insertMany([
+    {"color": "Black", "hex_color": "#000000"},
+    {"color": "Grey", "hex_color": "#bababa"},
+    {"color": "Dark grey", "hex_color": "#b2b2b2"},
+    {"color": "Orange", "hex_color": "#ffa500"},
+    {"color": "Dark navy", "hex_color": "#576675"}
+])
