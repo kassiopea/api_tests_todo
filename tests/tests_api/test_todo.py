e83a0cca363f927b04935a49e82df6ac381af725
randomnanastya <randomnanastya@gmail.com>
2021-05-23 23:44:13 +0300

    добавлены шаги и описание allure для тестов + удалена функция удаления пользователя. Теперь эта функциональность перенесена в конфтест

--- tests/tests_api/test_todo.py
+++ tests/tests_api/test_todo.py
@@ -7,21 +7,14 @@ from .messages import ErrorMessages
 from .data import todo_schema
 
 
-# функция для удаления пользователя после тестов
-def delete_user_after_test(auth_token, todo_list_api):
-    url = f'{AuthUrls.AUTH}{AuthUrls.DELETE}'
-    headers = {'Authorization': auth_token}
-    response = todo_list_api.delete(path=url, headers=headers)
-    with allure.step("Запрос отправлен. Проверяем, удалён ли пользователь."):
-        assert response.status_code == 200, f'Пользователь не был удалён. Status code is {response.status_code}'
-
-
+@allure.description('Проверяем, что не будет создана новая сущность todo без токена')
 class TestCheckRequestWithoutToken:
-
-    def test_check_status_code_after_adding_todo_without_token(self, todo_list_api, auth_token):
+    @allure.title('Проверить отправку запроса на создание todo без токена')
+    @allure.description('Проверяем, что не будет создана новая сущность todo без токена')
+    def test_check_status_code_after_adding_todo_without_token(self, todo_list_api_base_url):
         url = f'{TodoUrls.TODO_API}{TodoUrls.TODO}'
         data = {'description': 'test status code'}
-        response = todo_list_api.post(path=url, data=data)
+        response = todo_list_api_base_url.post(path=url, data=data)
         expected_status_code = 401
         actual_status_code = response.status_code
         with allure.step("Запрос отправлен. Проверяем код ответа."):
@@ -33,90 +26,99 @@ class TestCheckRequestWithoutToken:
             assert actual_headers == expected_headers
         with allure.step("Десериализируем ответ из json в словарь."):
             response_body = response.json()
-        with allure.step(f'Проверим, что в ответе {response_body} нам пришёл оджидаемый текст ошибки.'):
+        with allure.step('Проверим, что в ответе нам пришёл оджидаемый текст ошибки.'):
             expected_msg = ErrorMessages.AUTH_NONE_TOKEN
             actual_msg = response_body['msg']
             assert expected_msg == actual_msg, f'Текст ошибки {actual_msg} ' \
                                                f'не совпадает с ожидаемым текстом {expected_msg}'
 
-        with allure.step("Очищаем тестовые данные. Удаляем пользователя."):
-            delete_user_after_test(auth_token, todo_list_api)
-
 
 class TestCheckAddTodoWithOnlyRequiredFields:
-
-    def test_check_status_code_adding_todo_with_only_required_fields(self, todo_list_api, auth_token):
+    @allure.title('Проверяем код ответа на запрос с одним обязательным полем')
+    @allure.description('Проверяем код ответа на запрос создания todo только с одним обязательным полем')
+    def test_check_status_code_adding_todo_with_only_required_fields(self, todo_list_api_base_url, auth_token):
         url = f'{TodoUrls.TODO_API}{TodoUrls.TODO}'
         headers = {'Authorization': auth_token}
         data = {'description': 'test required field'}
-        response = todo_list_api.post(path=url, headers=headers, data=data)
-        assert response.status_code == 200
+        response = todo_list_api_base_url.post(path=url, headers=headers, data=data)
+        with allure.step('Проверяем, что код ответа равен 200'):
+            assert response.status_code == 200
 
-        delete_user_after_test(auth_token, todo_list_api)
-
-    def test_check_response_json_after_adding_todo_with_only_required_fields(self, auth_token, todo_list_api):
+    @allure.title('Проверяем content-type в ответе на запрос с одним обязательным полем')
+    @allure.description('Проверяем тип ответа на запрос создания todo только с одним обязательным полем')
+    def test_check_response_json_after_adding_todo_with_only_required_fields(self, auth_token, todo_list_api_base_url):
         url = f'{TodoUrls.TODO_API}{TodoUrls.TODO}'
         headers = {'Authorization': auth_token}
         data = {'description': 'test required field json'}
-        response = todo_list_api.post(path=url, headers=headers, data=data)
-        assert response.headers['Content-Type'] == "application/json"
+        response = todo_list_api_base_url.post(path=url, headers=headers, data=data)
+        with allure.step('Проверяем, что ответ пришёл в json формате.'):
+            assert response.headers['Content-Type'] == "application/json"
 
-        delete_user_after_test(auth_token, todo_list_api)
-
-    def test_check_keys_in_response_after_adding_todo_with_only_required_fields(self, auth_token, todo_list_api):
+    @allure.title('Проверяем, что есть необходимые ключи в ответе на запрос с одним обязательным полем')
+    @allure.description('Проверяем все ключи в ответе на запрос создания todo только с одним обязательным полем')
+    def test_check_keys_in_response_after_adding_todo_with_only_required_fields(self, auth_token,
+                                                                                todo_list_api_base_url):
         url = f'{TodoUrls.TODO_API}{TodoUrls.TODO}'
         headers = {'Authorization': auth_token}
         data = {'description': 'test required field keys'}
-        response = todo_list_api.post(path=url, headers=headers, data=data)
+        response = todo_list_api_base_url.post(path=url, headers=headers, data=data)
         response_body = response.json()
         response_keys = response_body.keys()
-        assert response_keys == {'data', 'errors', 'status'}
-        response_data = response_body['data']
-        response_key = response_data.keys()
-        assert response_key == {'todo_id'}
+        expected_keys = {'data', 'errors', 'status'}
+        with allure.step(f'Проверяем, что в ответе есть ключи {expected_keys}'):
+            assert response_keys == expected_keys
 
-        delete_user_after_test(auth_token, todo_list_api)
-
-    def test_check_id_in_response_after_adding_todo_with_only_required_fields(self, auth_token, todo_list_api):
+    @allure.title('Проверяем, что есть ключ с id todo в ответе на запрос с одним обязательным полем')
+    @allure.description('Проверяем наличие ключа todo_id в data в ответе на запрос создания todo '
+                        'только с одним обязательным полем')
+    def test_check_id_in_response_after_adding_todo_with_only_required_fields(self, auth_token, todo_list_api_base_url):
         url = f'{TodoUrls.TODO_API}{TodoUrls.TODO}'
         headers = {'Authorization': auth_token}
         data = {'description': 'test required field keys'}
-        response = todo_list_api.post(path=url, headers=headers, data=data)
+        response = todo_list_api_base_url.post(path=url, headers=headers, data=data)
         response_body = response.json()
         response_data = response_body['data']
         response_key = response_data.keys()
-        assert response_key == {'todo_id'}
+        expected_key = {'todo_id'}
+        with allure.step(f'Проверяем, что data содержит ключ {expected_key}'):
+            assert response_key == expected_key
 
-        delete_user_after_test(auth_token, todo_list_api)
-
-    def test_check_schema_todo_with_only_description(self, auth_token, todo_list_api):
+    @allure.title('Проверяем json схему ответа')
+    @allure.description('Валидируем json схему ответа на запрос о создании todo с одним обязательным полем')
+    def test_check_schema_todo_with_only_description(self, auth_token, todo_list_api_base_url):
         url = f'{TodoUrls.TODO_API}{TodoUrls.TODO}'
         headers = {'Authorization': auth_token}
         data = {'description': 'test required field todo'}
-        response = todo_list_api.post(path=url, headers=headers, data=data)
+        response = todo_list_api_base_url.post(path=url, headers=headers, data=data)
         # проверяем схему ответа
         response_body = response.json()
-        assert schema(todo_schema.schema_to_respond_to_post_request_to_add_todo) == response_body
+        with allure.step('Проверяем, что ответ совпадает с ожидаемой json схемой'):
+            assert schema(todo_schema.schema_to_respond_to_post_request_to_add_todo) == response_body
 
-        delete_user_after_test(auth_token, todo_list_api)
-
+    @allure.title(f'Проверяем, что todo создан c ожидаемым описанием')
+    @allure.description('Проверяем, что можем создать todo с минимальной, максимальной и средней длинной '
+                        'поля "Описание". А также проверяем, что созданная сущность соответствует той, что приходит'
+                        'по get запросу на получение одной сущности')
     @pytest.mark.parametrize("todo", testdata)
-    def test_check_adding_todo_with_valid_description(self, auth_token, todo_list_api, todo):
+    def test_check_adding_todo_with_valid_description(self, auth_token, todo_list_api_base_url, todo):
         url = f'{TodoUrls.TODO_API}{TodoUrls.TODO}'
         headers = {'Authorization': auth_token}
-        # data = {'description': 'test required field todo'}
         data = vars(todo)
-        response = todo_list_api.post(path=url, headers=headers, data=data)
+        response = todo_list_api_base_url.post(path=url, headers=headers, data=data)
         response_body = response.json()
         todo_id = response_body['data']['todo_id']
-
-        # проверяем, что сущность сохранилась
-        url_for_getting_todo_list = f'{TodoUrls.TODO_API}{TodoUrls.TODO}'
-        params = {'todo_id': todo_id}
-        response_todo = todo_list_api.get(path=url_for_getting_todo_list, params=params, headers=headers)
-        response_todo_body = response_todo.json()
-        actual_id = response_todo_body['data'][0]['_id']
-        assert actual_id == todo_id
-        assert response_todo_body['data'][0]['description'] == data['description']
-
-        delete_user_after_test(auth_token, todo_list_api)
+        with allure.step(f'Создали todo c описанием {todo.description}.'):
+            assert response.status_code == 200, f"Todo не была создана. Код ответа {response.status_code}"
+        with allure.step('Отправляем запрос на получение информации о только что созданной todo'):
+            # проверяем, что сущность сохранилась
+            url_for_getting_todo_list = f'{TodoUrls.TODO_API}{TodoUrls.TODO}'
+            params = {'todo_id': todo_id}
+            response_todo = todo_list_api_base_url.get(path=url_for_getting_todo_list, params=params, headers=headers)
+            response_todo_body = response_todo.json()
+            actual_id = response_todo_body['data'][0]['_id']
+            with allure.step(f'Проверяем, что в ответе id todo соответствует ожидаемому'):
+                assert actual_id == todo_id
+            expected_description = data['description']
+            actual_description = response_todo_body['data'][0]['description']
+            with allure.step(f'Проверяем, что в ответе описание соответствует ожидаемому'):
+                assert actual_description == expected_description
