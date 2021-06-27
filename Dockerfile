25c1b1b28afb970db1ce3f539feb05b358b7d583
randomnanastya <randomnanastya@gmail.com>
2021-06-27 18:06:29 +0300

    fixed bag in mongo-init plus edited dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,10 @@
 FROM python:3.8-slim
-#FROM ubuntu:latest
-#
-#RUN apt-get update && \
-#    apt-get install --no-install-recommends -y \
-#    python3.8 python3-pip python3.8-dev
-RUN mkdir -p var/www/app
-RUN cd var/www/app
+RUN mkdir -p var/www/app && cd var/www/app
+#RUN cd var/www/app
 RUN groupadd -r tester && useradd -r -s /bin/false -g tester tester
 WORKDIR var/www/app
 COPY requirements.txt .
-RUN pip install -r requirements.txt
-RUN chown -R tester:tester /var/www/app
+RUN pip install -r requirements.txt && chown -R tester:tester /var/www/app
+#RUN chown -R tester:tester /var/www/app
 USER tester
 COPY . .
--- mongo/mongo-init.js
+++ mongo/mongo-init.js
@@ -1,13 +1,13 @@
 db = db.getSiblingDB('DevTodoDB');
-db.dropDatabase()
-db.createUser(
-  {
-    user: 'test_api_user',
-    email: 'test_api@test.ru'
-    pwd: '123456',
-    roles: [{ role: 'readWrite', db: 'DevTodoDB' }],
-  },
-);
+db.dropDatabase();
+//db.createUser(
+//  {
+//    user: 'test_api_user',
+//    email: 'test_api@test.ru'
+//    pwd: '123456',
+//    roles: [{ role: 'readWrite', db: 'DevTodoDB' }],
+//  },
+//);
 
 db.createCollection('colors');
 
@@ -17,4 +17,4 @@ db.colors.insertMany([
     {"color": "Dark grey", "hex_color": "#b2b2b2"},
     {"color": "Orange", "hex_color": "#ffa500"},
     {"color": "Dark navy", "hex_color": "#576675"}
-])
+]);
